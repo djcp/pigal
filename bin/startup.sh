01de2608d90839faed764b64a7dce5d0d84db5f0
djcp <djcp@thoughtbot.com>
2012-12-28 11:56:42 -0500

    Transition speed, startup fix

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,4 +3,4 @@
 cd /home/pi/code/pigal/
 source /home/pi/.rvm/scripts/rvm
 
-rackup config.ru -D -P /home/pi/code/pigal/tmp/pigal.pid config.ru
+rackup -P /home/pi/code/pigal/tmp/pigal.pid config.ru
