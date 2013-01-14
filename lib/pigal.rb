ba747ef8a0daf90a27bccd2845aec9d7dc2f8d94
Dan Collis-Puro <djcp@thoughtbot.com>
2013-01-13 21:51:21 -0500

    URLs are no longer encoded in the ajax request

--- lib/pigal.rb
+++ lib/pigal.rb
@@ -15,15 +15,12 @@ class Pigal < Sinatra::Base
 
   get '/get_proxied_json' do
     content_type :json
-
-    jpd = JsonProxyDispatcher.new(get_urls)
+    jpd = JsonProxyDispatcher.new(config['urls'])
     jpd.as_json
   end
 
   get '/pigal.js' do
     content_type 'application/javascript'
-
-    @encoded_source_urls = get_json_proxy_urls
     erb 'pigal.js'.to_sym
   end
 
@@ -33,15 +30,4 @@ class Pigal < Sinatra::Base
     @config || YAML.load_file('./config.yml')
   end
 
-  def get_urls
-    [params[:urls]].flatten.compact
-  end
-
-  def get_json_proxy_urls
-    output = ''
-    config['urls'].each do |url|
-      output += "urls[]=#{CGI.escape(url)}&"
-    end
-    output
-  end
 end
